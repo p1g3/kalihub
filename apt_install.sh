22098537677ec5c30ca6798541a4e55097741b91
nu11hex <nu11hex@protonmail.com>
2019-10-28 22:12:45 +0800

    Fixed_errors

--- apt_install.sh
+++ apt_install.sh
@@ -5,13 +5,13 @@ apt install -y apt-transport-https
 
 # Execute
 apt install fcitx fcitx-pinyin aria2 tor telegram-desktop \
- virtualbox wine32 playonlinux redis-server android-tools-adb fastboot \
+ virtualbox wine64 playonlinux redis-server android-tools-adb fastboot \
  gimp audacity ffmpeg obs-studio mesa-utils \
- zaproxy veil shellter bdfproxy strace \
+ zaproxy veil shellter bdfproxy strace gdb \
 && /usr/share/veil/config/setup.sh --force --silent \
 && bash <(curl -L -s https://install.direct/go.sh)
 # && bash <(curl -L -s https://get.acme.sh) \
 # && bash <(curl -sLf https://spacevim.org/install.sh) \
 # && bash <(curl -s https://get.docker.com) \
-# pip3 install docker-compose pwn zio
+# pip install docker-compose pwn zio
 # Notice: VSCode, AntSword, IDA, IDEA
