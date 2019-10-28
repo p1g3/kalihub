2af29a8e0e627612896443099620547024d0d13c
nu11hex <nu11hex@protonmail.com>
2019-10-28 23:22:08 +0800

    Fixed_errore

--- apt_install.sh
+++ apt_install.sh
@@ -7,11 +7,13 @@ apt install -y apt-transport-https
 apt install fcitx fcitx-pinyin aria2 tor telegram-desktop \
  virtualbox wine64 playonlinux redis-server android-tools-adb fastboot \
  gimp audacity ffmpeg obs-studio mesa-utils \
- zaproxy veil shellter bdfproxy strace gdb \
+ zaproxy veil shellter bdfproxy strace \
+ python-pip libssl-dev \
 && /usr/share/veil/config/setup.sh --force --silent \
 && bash <(curl -L -s https://install.direct/go.sh)
 # && bash <(curl -L -s https://get.acme.sh) \
 # && bash <(curl -sLf https://spacevim.org/install.sh) \
 # && bash <(curl -s https://get.docker.com) \
-# pip install docker-compose pwn zio
+# pip install pwntools zio
+# pip3 install docker-compose
 # Notice: VSCode, AntSword, IDA, IDEA
