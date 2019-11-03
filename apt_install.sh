4650db7714a009a5e803e929f69e6ea8d83a3ece
nu11hex <nu11hex@protonmail.com>
2019-11-03 16:32:36 +0800

    Refed_apt_install

--- apt_install.sh
+++ apt_install.sh
@@ -4,16 +4,16 @@
 apt install -y apt-transport-https
 
 # Execute
-apt install fcitx fcitx-pinyin aria2 tor telegram-desktop \
- virtualbox wine64 playonlinux redis-server android-tools-adb fastboot \
- gimp audacity ffmpeg obs-studio mesa-utils \
- veil shellter bdfproxy strace \
- zaproxy python-pip gdb libssl-dev \
+apt install fcitx fcitx-pinyin virtualbox wine64 playonlinux \
+ aria2 tor telegram-desktop gimp audacity ffmpeg obs-studio mesa-utils \
+ php-fpm php7.3-fpm python-pip libssl-dev redis-server android-tools-adb \
+ zaproxy gdb foremost fastboot strace \
+ veil shellter bdfproxy \
 && /usr/share/veil/config/setup.sh --force --silent \
 && bash <(curl -L -s https://install.direct/go.sh)
 # && bash <(curl -L -s https://get.acme.sh) \
 # && bash <(curl -sLf https://spacevim.org/install.sh) \
 # && bash <(curl -s https://get.docker.com) \
-# pip install pwntools zio
+pip install pwntools zio
 # pip3 install pyjwt docker-compose
-# Notice: VSCode, AntSword, IDA, IDEA, node.js
+# Notice: VSCode, AntSword, IDA, IDEA, node.js, crossover
